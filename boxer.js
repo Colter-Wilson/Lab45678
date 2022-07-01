6b299bee72ec837c6603a3164a8c397a175960f0
Colter-Wilson <itscolter@gmail.com>
2022-06-30 23:31:59 -0400

    added new question

--- boxer.js
+++ boxer.js
@@ -2,3 +2,6 @@ let favoriteBoxer = prompt('Who is your favorite boxer?');
 let message = "Hmmm, " + favoriteBoxer + ", that's a good one!";
 alert(message);
 document.getElementById('favorite-boxer').innerHTML = favoriteBoxer;
+let potato = prompt('What is your favorite color?')
+let message2 = potato + "is a great color!"
+alert(message2)
