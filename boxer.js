1e6322a98bc5fe20e61aab57dbe756b18fcf3dc6
Colter-Wilson <itscolter@gmail.com>
2022-07-01 14:34:45 -0400

    "Changed JS to give error message when no color is entered"

--- boxer.js
+++ boxer.js
@@ -1,7 +1,19 @@
-let favoriteBoxer = prompt('Who is your favorite boxer?');
-let message = "Hmmm, " + favoriteBoxer + ", that's a good one!";
-alert(message);
-document.getElementById('favorite-boxer').innerHTML = favoriteBoxer;
-let potato = prompt('What is your favorite color?')
-let message2 = potato + "is a great color!"
-alert(message2)
+function getFavoriteBoxer() {
+    let favoriteBoxer = prompt('Who is your favorite boxer?');
+    let message = "Hmmm, " + favoriteBoxer + ", that's a good one!";
+    alert(message);
+    document.getElementById('favorite-boxer').innerHTML = favoriteBoxer;
+}
+
+function getFavoriteColor() {
+    let favoriteColor = prompt('What is your favorite color?');
+    let message = "Hmmm, " + favoriteColor + ", that's a great color!";
+    alert(message);
+    if (favoriteColor == '') {
+         prompt('Lets try that again... What is your favorite color?');
+    }
+    document.getElementById('favorite-color').innerHTML = favoriteColor;
+}
+
+getFavoriteBoxer()
+getFavoriteColor()
